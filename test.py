5188e0b5df04a3761aee3bff0412175b00df08a2
Benjamin Congdon <bencongdon96@gmail.com>
2020-01-06 11:15:38 -0800

    Add support for remesh operation

--- test.py
+++ test.py
@@ -2,7 +2,16 @@ from bpy_lambda import bpy
 
 
 def handler(event, context):
-    bpy.ops.mesh.primitive_cube_add(location=(0, 0, 0))
-    cube = bpy.context.scene.objects.active
-    cube.scale = (1, 2, 3)
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete()
+    bpy.ops.mesh.primitive_uv_sphere_add()
+    bpy.ops.object.modifier_add(type='REMESH')
+    bpy.context.object.modifiers["Remesh"].octree_depth = 1
+    bpy.context.object.modifiers["Remesh"].scale = 0.9
+    bpy.context.object.modifiers["Remesh"].threshold = 1
+    bpy.context.object.modifiers["Remesh"].sharpness = 0
+    bpy.context.object.modifiers["Remesh"].mode = 'SMOOTH'
+    bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Remesh")
+    # Test REMESH operation
+    assert(len(bpy.context.object.data.vertices) == 8)
     return {'status': 'ok'}
--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-docker run --rm -v "$PWD":/var/task lambci/lambda:python3.6 test.handler
+docker run --rm -u root -v "$PWD":/var/task:ro,delegated lambci/lambda:python3.6 test.handler
